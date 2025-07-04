2965efc09ec6830ec3e4fbb4ae650168e37dda1b
Nicollas <nicollaslira0@gmail.com>
2025-07-04 10:07:12 -0300

    Exercício 1 – primeira versão

---
+++ Exercicio 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome = "Nicollas Lira";
+    int idade = 17;
+    string frase = "ser ou nao ser, eis a questao";
+
+    cout << nome << endl;
+    cout << idade << endl;
+    cout << frase << endl;
+
+    cin.get(); 
+
+    return 0;
+}
